73bbf45ba7dd73eab8d3ea43c304ad6c9c2373b8
jenar-k <jenarkuswidiardi@gmail.com>
2021-08-03 10:17:10 +0700

    tambah file

---
+++ latihan1.js
@@ -0,0 +1,43 @@
+/*
+alert('selamat datang di kelas ini ...');
+var nama = prompt('masukkan nama kamu : ');
+document.write('Selamat datang Kak ' + nama + '<br>');
+document.write('Silahkan belajar dengan penuh semangat ..!!' +'<br>');
+document.write(5+5 +'<br>');
+*/
+
+document.write('Selamat datang Kak Jenar' +'<br>');
+document.write('Silahkan belajar dengan penuh semangat ..!!' +'<br>');
+/*
+var bil1 = parseInt(prompt('masukkan bilangan pertama'));
+var bil2 = parseInt(prompt('masukkan bilangan kedua'));
+*/
+
+/*
+var bil1 = 20;
+var bil2 = 20;
+var bil3 = bil1 + bil2;
+//document.write('Hasil Penjumlahan ' +bil3 +'<br>');
+document.write('bilangan pertama adalah ' + bil1 +'<br>');
+document.write('bilangan kedua adalah ' + bil2 +'<br>');
+document.write(bil1 > bil2); //false
+document.write('<br>');
+document.write(bil1 < bil2); //true
+document.write('<br>');
+document.write(!(bil1 > bil2 || bil1 < bil2));
+*/
+
+/*
+var nilai = 75;
+var kelulusan = (nilai>70)? 'lulus' : 'remedial'
+document.write(kelulusan);
+*/
+
+var konfirmasi = confirm('apakah Anda ingin mengakhiri kelas?');
+document.write(konfirmasi + '<br>')
+
+//pop up ada
+// alert = OK
+// promtp = String mengisikan nilai 
+// confirm = Yes No
+
